78d17635258fc5e86eb140043cf686f5ebdf4ea1
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-19 19:19:56 +0530

    Update DeckOfCards.py

--- DeckOfCards.py
+++ DeckOfCards.py
@@ -1,6 +1,9 @@
 import itertools, random
+
 # product() function, performs the Cartesian product of the two sequences.
-# The two sequences are numbers from 1 to 13 and the four suits. So, altogether we have 13 * 4 = 52 items in the deck with each card as a tuple
+# The two sequences are numbers from 1 to 13 and the four suits.
+# i.e 13 * 4 = 52 items in the deck with each card as a tuple
+
 deck = list(itertools.product(range(1,14),["Spade", "Heart", "Diamond", "Club"]))
 random.shuffle(deck)
 print("You have got :")
